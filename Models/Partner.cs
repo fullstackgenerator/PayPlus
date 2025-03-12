a3ee2118293ae4e6c4143ca4a6500a755444e956
fullstackgenerator <simon.hojnik@gmail.com>
2025-03-12 21:34:02 +0100

    added crud

---
+++ Models/Partner.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PayPlus.Models
+{
+    public class Partner
+    {
+        public required int Id { get; set; }
+        
+        [Display(Name = "Ime")]
+        public required string Name { get; set; }
+        
+        [Display(Name = "Davčna številka")]
+        public required int VAT_number { get; set; }
+        
+        [Display(Name = "Naslov")]
+        public required string Address { get; set; }
+        
+        [Display(Name = "Poštna številka")]
+        public required int Postal_code { get; set; }
+        
+        [Display(Name = "Kraj")]
+        public required string City { get; set; }
+        
+        [Display(Name = "Država")]
+        public required string Country { get; set; }
+    }
+}
