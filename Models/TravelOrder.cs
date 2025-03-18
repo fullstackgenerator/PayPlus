90afe14ac1c17758de99d632ebef691b2b6170ab
fullstackgenerator <simon.hojnik@gmail.com>
2025-03-18 16:13:17 +0100

    travel model model update #2

--- Models/TravelOrder.cs
+++ Models/TravelOrder.cs
@@ -2,35 +2,38 @@
 
 namespace PayPlus.Models;
 
+public enum CarType
+{
+    PrivateCar,
+    CompanyCar
+    
+}
+
 public class TravelOrder
 {
     public required int order_id { get; set; }
-    
+
     [DisplayName("Datum dokumenta:")]
     public required DateTime order_date { get; set; }
-    
+
     [DisplayName("Datum začetka potovanja:")]
     public required DateTime start_date { get; set; }
-    
+
     [DisplayName("Datum zaključka potovanja:")]
     public required DateTime end_date { get; set; }
-    
-    [DisplayName("V/na:")]
-    public required string start_location { get; set; }
-    
-    [DisplayName("Ime voznika:")]
-    public required string full_name_driver { get; set; }
-    
-    [DisplayName("Ime sovoznika:")]
-    public string full_name_passenger { get; set; }
-    
-    [DisplayName("Ime drugega sovoznika:")]
-    public string full_name_passenger_two { get; set; }
 
-    [DisplayName("Ime tretjega sovoznika:")]
-    public string full_name_passenger_three { get; set; }
-    
-    [DisplayName("Ime četrtega sovoznika:")]
-    public string full_name_passenger_four { get; set; }
-    
+    [DisplayName("V/na:")]
+    public required string location { get; set; }
+
+    [DisplayName("Ime voznika:")]
+    public required string driver_full_name { get; set; }
+
+    [DisplayName("Znamka in model avtomobila:")]
+    public required string car_brand_and_model { get; set; }
+
+    [DisplayName("Razlog potovanja:")]
+    public required string trip_reason { get; set; }
+
+    [DisplayName("Odobravam uporabo:")]
+    public required CarType car_type { get; set; }
 }
